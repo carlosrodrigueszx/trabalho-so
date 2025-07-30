5d4d30792acd9c0a6fa1f22ce4e6eb53ee35f5a4
murfra <murilotutoriais1000@hotmail.com>
2025-07-29 21:20:02 -0300

    Time changes

--- Q1.c
+++ Q1.c
@@ -16,95 +16,95 @@ pthread_cond_t cond_cheio = PTHREAD_COND_INITIALIZER;
 pthread_cond_t cond_vazio = PTHREAD_COND_INITIALIZER;
 
 void *producer(void *args) {
-    pthread_t tid = pthread_self();
+  pthread_t tid = pthread_self();
 
-    int iteracoes;
-    do {
-        iteracoes = (rand() % 11) + 20;
-    } while (iteracoes % 5 != 0);
+  int iteracoes;
+  do {
+    iteracoes = (rand() % 11) + 20;
+  } while (iteracoes % 5 != 0);
 
-    for (int i = 0; i < iteracoes; i++) {
-        float valor = (rand() % 1000) + 1;
-
-        pthread_mutex_lock(&mutex);
-        while (count == BUFFER_SIZE)
-            pthread_cond_wait(&cond_vazio, &mutex);
-
-        buffer[in] = valor;
-        int pos = in;
-        in = (in + 1) % BUFFER_SIZE;
-        count++;
-
-        pthread_cond_signal(&cond_cheio);
-
-        printf("(P) TID: %ld | VALOR: R$ %.0f | ITERACAO: %d\n", tid, valor, i + 1);
-
-        pthread_mutex_unlock(&mutex);
-
-        sleep(rand() % 5 + 1);
-    }
+  for (int i = 0; i < iteracoes; i++) {
+    float valor = (rand() % 1000) + 1;
 
     pthread_mutex_lock(&mutex);
-    produtoras_finalizadas++;
+    while (count == BUFFER_SIZE)
+        pthread_cond_wait(&cond_vazio, &mutex);
+
+    buffer[in] = valor;
+    int pos = in;
+    in = (in + 1) % BUFFER_SIZE;
+    count++;
+
     pthread_cond_signal(&cond_cheio);
+
+    printf("(P) TID: %ld | VALOR: R$ %.0f | ITERACAO: %d\n", tid, valor, i + 1);
+
     pthread_mutex_unlock(&mutex);
 
-    printf("(P) TID: %ld finalizou\n", tid);
+    sleep(rand() % 5 + 1);
+  }
 
-    return NULL;
+  pthread_mutex_lock(&mutex);
+  produtoras_finalizadas++;
+  if (count == BUFFER_SIZE) pthread_cond_signal(&cond_cheio);
+  pthread_mutex_unlock(&mutex);
+
+  printf("(P) TID: %ld finalizou\n", tid);
+
+  return NULL;
 }
 
 void *consumer(void *args) {
-    pthread_t tid = pthread_self();
-    int iteracao = 1;
+  pthread_t tid = pthread_self();
+  int iteracao = 1;
 
-    while (1) {
-        pthread_mutex_lock(&mutex);
+  while (1) {
+    pthread_mutex_lock(&mutex);
 
-        while (count < BUFFER_SIZE && produtoras_finalizadas < NUM_PRODUTORAS)
-            pthread_cond_wait(&cond_cheio, &mutex);
+    while (count < BUFFER_SIZE && produtoras_finalizadas < NUM_PRODUTORAS)
+      pthread_cond_wait(&cond_cheio, &mutex);
 
-        if (count < BUFFER_SIZE && produtoras_finalizadas == NUM_PRODUTORAS) {
-            pthread_mutex_unlock(&mutex);
-            break;
-        }
-
-        float valores[BUFFER_SIZE];
-        float soma = 0;
-
-        for (int i = 0; i < BUFFER_SIZE; i++) {
-            int idx = (in - count + BUFFER_SIZE + i) % BUFFER_SIZE;
-            valores[i] = buffer[idx];
-            soma += valores[i];
-        }
-
-        count = 0;
-        pthread_cond_broadcast(&cond_vazio);
-        pthread_mutex_unlock(&mutex);
-
-        printf("(C) TID: %ld | MEDIA: R$ %.0f | ITERACAO: %d\n", tid, soma / BUFFER_SIZE, iteracao++);
+    if (count < BUFFER_SIZE && produtoras_finalizadas == NUM_PRODUTORAS) {
+      pthread_mutex_unlock(&mutex);
+      break;
     }
 
-    printf("(C) TID: %ld finalizou\n", tid);
-    return NULL;
+    float valores[BUFFER_SIZE];
+    float soma = 0;
+
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+      int idx = (in - count + BUFFER_SIZE + i) % BUFFER_SIZE;
+      valores[i] = buffer[idx];
+      soma += valores[i];
+    }
+
+    count = 0;
+    pthread_cond_broadcast(&cond_vazio);
+    pthread_mutex_unlock(&mutex);
+
+    printf("(C) TID: %ld | MEDIA: R$ %.0f | ITERACAO: %d\n", tid, soma / BUFFER_SIZE, iteracao++);
+  }
+
+  printf("(C) TID: %ld finalizou\n", tid);
+  return NULL;
 }
 
 int main(void) {
-    srand(time(NULL));
+  srand(time(NULL));
 
-    pthread_t produtoras[NUM_PRODUTORAS];
-    pthread_t consumidor;
+  pthread_t produtoras[NUM_PRODUTORAS];
+  pthread_t consumidor;
 
-    for (int i = 0; i < NUM_PRODUTORAS; i++) {
-        pthread_create(&produtoras[i], NULL, producer, NULL);
-    }
+  for (int i = 0; i < NUM_PRODUTORAS; i++) {
+    pthread_create(&produtoras[i], NULL, producer, NULL);
+  }
 
-    pthread_create(&consumidor, NULL, consumer, NULL);
+  pthread_create(&consumidor, NULL, consumer, NULL);
 
-    for (int i = 0; i < NUM_PRODUTORAS; i++)
-        pthread_join(produtoras[i], NULL);
+  for (int i = 0; i < NUM_PRODUTORAS; i++)
+    pthread_join(produtoras[i], NULL);
 
-    pthread_join(consumidor, NULL);
+  pthread_join(consumidor, NULL);
 
-    return 0;
+  return 0;
 }
--- Q2.c
+++ Q2.c
@@ -1,16 +1,16 @@
+#include <bits/time.h>
 #include <pthread.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 #define NUM_TERMS 2000000000
-#define NUM_THREADS 16
+#define NUM_THREADS 8
 #define PARTIAL_NUM_TERMS ((NUM_TERMS)/(NUM_THREADS))
 
-double total_pi = 0;
+double thrs_sum = 0;
 double total_thr_time = 0;
-pthread_mutex_t lock; 
-pthread_mutex_t time_lock; 
+pthread_mutex_t lock, time_lock; 
 
 double partialFormula(int first_therm) {
   // A funcao ira processar PARTIAL_NUM_TERMS termos
@@ -30,20 +30,24 @@ double partialFormula(int first_therm) {
 
 void* partialProcessing(void* args) {
   int first_therm = *((int*) args);
-  pthread_t current_thread_id = pthread_self();
-  
+  free(args);
+
   // obter tempo de inicio
-  clock_t start_time = clock();
+  struct timespec t0, t1;
+  clock_gettime(CLOCK_MONOTONIC, &t0);
+  // clock_t start_time = clock();
   double sum = partialFormula(first_therm);
 
   // acessar buffer compartilhado
   pthread_mutex_lock(&lock);
-  total_pi += sum;
+  thrs_sum += sum;
   pthread_mutex_unlock(&lock);
 
   // obter tempo de fim
-  clock_t end_time = clock();
-  double total_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
+  clock_gettime(CLOCK_MONOTONIC, &t1);
+  // clock_t end_time = clock();
+  double total_time = (t1.tv_sec  - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec)*1e-9;
+  // double total_time2 = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
 
   // acessar buffer compartilhado de tempo
   pthread_mutex_lock(&time_lock);
@@ -51,53 +55,63 @@ void* partialProcessing(void* args) {
   pthread_mutex_unlock(&time_lock);
 
   // mostrar TID e tempo empregado
-  printf("TID: %lu: %.2fs\n", (unsigned long) current_thread_id, total_time);
+  printf("TID: %lu: %.2fs\n", (unsigned long) pthread_self(), total_time);
+  // printf("TID: %lu: clock(): %.2fs\n", (unsigned long) pthread_self(), total_time2);
   pthread_exit(NULL);
 }
 
 int main(void) {
   double f_pi = 0, s_pi = 0; 
+  struct timespec m0, m1;
+
   clock_t start_time = clock(); // Captura o tempo inicial
+  // clock_gettime(CLOCK_MONOTONIC, &m0); // wall-clock
   double signal = 1.0;
   for (int k = 0; k < NUM_TERMS; k++) {
     f_pi += signal / (2 * k + 1);
     signal *= -1.0;
   }
   f_pi *= 4;
+  // clock_gettime(CLOCK_MONOTONIC, &m1);
   clock_t end_time = clock(); // Captura o tempo final
 
   // Calcula o tempo de execução em segundos
   double total_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
+  // double gettime = (m1.tv_sec  - m0.tv_sec) + (m1.tv_nsec - m0.tv_nsec)*1e-9;
   printf("Tempo Processo (Sequencial): %.2fs\n", total_time);
-  printf("pi (Sequencial) = %.9f\n", f_pi);
+  // printf("Tempo Processo Gettime(Sequencial): %.2fs\n", gettime);
 
   pthread_t threads[NUM_THREADS];
-  pthread_mutex_init(&lock, NULL);      // inicializa o mutex que protege o total_pi
+  pthread_mutex_init(&lock, NULL);      // inicializa o mutex que protege o thrs_sum
   pthread_mutex_init(&time_lock, NULL); // inicializa o mutex que protege o total_thr_time
 
   // obter tempo de inicio
   start_time = clock();
+  // clock_gettime(CLOCK_MONOTONIC, &m0);
   for (int t = 0; t < NUM_THREADS; t++) {
     int* arg = malloc(sizeof(int));
     *arg = t * PARTIAL_NUM_TERMS;
     // criar threads parciais
-    pthread_create(&threads[t], NULL, partialProcessing, arg);
+    pthread_create(&threads[t], NULL, &partialProcessing, arg);
   }
 
   // esperar threads terminarem
   for (int t = 0; t < NUM_THREADS; t++) {
     pthread_join(threads[t], NULL);
   }
-  s_pi = total_pi * 4;
+  s_pi = thrs_sum * 4;
 
   // obter tempo de fim
   end_time = clock();
   total_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
+  // clock_gettime(CLOCK_MONOTONIC, &m1);
+  // gettime = (m1.tv_sec  - m0.tv_sec) + (m1.tv_nsec - m0.tv_nsec)*1e-9;
+  // printf("Tempo Processo Gettime(paralelo): %.2fs\n", gettime);
 
   // mostrar resultado e tempo empregado
   printf("Total Processo (Paralelo): %.2fs\n", total_time);
   printf("Total Threads: %.2fs\n", total_thr_time);
-  printf("pi (Paralelo) = %.9f\n", s_pi);
+  printf("pi (Sequencial) = %.9f, pi (Paralelo) = %.9f\n", f_pi, s_pi);
 
   pthread_mutex_destroy(&lock);
   pthread_mutex_destroy(&time_lock);
